9804a879a704daeefa39a8c06afc8d7c17cd57ac
Maxim Tokmakov <roentahl@gmail.com>
2022-11-23 18:38:05 +0300

    recreate app, add createPagination, createFilmCard, createApp getFilmData functions

--- js/main.js
+++ js/main.js
@@ -1,129 +1,156 @@
-// global const
-const API_KEY = 'cda4fa97-c203-4818-b207-28de27eb0201';
-
-
-// get query-params
-const params = new Proxy(new URLSearchParams(window.location.search), {
-  get: (searchParams, prop) => searchParams.get(prop)
-});
-
-let queryParam = params.page;
-
-if (!queryParam) {
-  queryParam = 1;
-  history.pushState(null, null, '?page=1');
-};
-
-// Month arrays
-const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-const monthRu = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
-
-const currentYear = new Date().getFullYear();
-const currentMonth = new Date().getMonth();
-const currentMonthText = month[currentMonth].toUpperCase();
-const currentMonthTextRu = monthRu[currentMonth];
-
-// DOM elements
-const appList = document.querySelector('.app__list');
-const spanMonth = document.querySelector('.month');
-const spanYear = document.querySelector('.year');
-const paginations = document.querySelectorAll('.app__pagination');
-const errorEl = document.querySelector('.app__error');
-const loaderEl = document.querySelector('.app__loader');
-
 // declension of numbers
 const seclOfNum = (number, titles) => {
   let cases = [2, 0, 1, 1, 1, 2];
   return titles[ (number%100>4 && number%100<20) ? 2 : cases[ (number%10<5) ? number%10:5 ] ];
 };
 
-// set month
-spanMonth.textContent = currentMonthTextRu;
+function createPagination(totalPages, queryParam) {
+  const pagination = document.createElement('ul');
+  pagination.classList.add('list-reset', 'app__pagination');
 
-// set year
-spanYear.textContent = currentYear;
+  const pages = Math.ceil(totalPages / 10);
 
-const initApp = (page) => {
-  const url = `https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=${currentYear}&month=${currentMonthText}&page=${page}`;
+  for (let i = 1; i <= pages; i++) {
+    const paginationItem = document.createElement('li');
+    const paginationLink = document.createElement('a');
 
-  fetch(url, {
+    paginationItem.classList.add('app__pagination-item');
+    paginationLink.classList.add('app__pagination-link');
+    paginationLink.href = `?page=${i}`;
+    paginationLink.textContent = i;
+
+    if (i == queryParam) {
+      paginationLink.classList.add('app__pagination-link--current');
+    }
+
+    paginationItem.append(paginationLink);
+
+    pagination.append(paginationItem);
+  };
+
+  return pagination;
+};
+
+function createFilmCard({ filmId, nameRu, nameEn, rating, genres, duration, releaseDate, posterUrlPreview }) {
+  const filmCard = document.createElement('article');
+  const filmLink = document.createElement('a');
+  const filmImgWrap = document.createElement('div');
+  const filmPoster = document.createElement('img');
+  const filmDetails = document.createElement('div');
+  const filmRating = document.createElement('div');
+  const filmGenres = document.createElement('div');
+  const filmDuration = document.createElement('div');
+  const filmTitle = document.createElement('h2');
+  const filmReleaseDate = document.createElement('div');
+
+  filmCard.classList.add('app__card', 'film__card');
+  filmLink.classList.add('film-card__link');
+  filmImgWrap.classList.add('film-card__img-wrap');
+  filmPoster.classList.add('film-card__img');
+  filmDetails.classList.add('film-card__details');
+  filmRating.classList.add('film-card__rating');
+  filmGenres.classList.add('film-card__genres');
+  filmDuration.classList.add('film-card__duration');
+  filmTitle.classList.add('film-card__title');
+  filmReleaseDate.classList.add('film-card__release-date');
+
+  if (rating == null) {
+    filmRating.classList.add('film-card__rating--null');
+  }
+
+  filmLink.href = `https://kinopoisk.ru/film/${filmId}`;
+  filmLink.target = '_blank';
+
+  filmPoster.src = posterUrlPreview;
+  filmPoster.alt = nameRu ? nameRu : nameEn;
+  filmPoster.loading = 'lazy';
+
+  filmRating.textContent = rating ? rating.toFixed(1) : 'Недостаточно голосов';
+
+  filmGenres.textContent = genres.map(genre => Object.values(genre)[0]).toString().replace(/,/g, ', ');
+
+  if (duration == 0) {
+    filmDuration.classList.add('film-card__duration--hidden')
+  } else {
+    filmDuration.textContent = `${duration} ${seclOfNum(duration, ['минута', 'минуты', 'минут'])}`;
+  }
+
+  filmTitle.textContent = nameRu ? nameRu : nameEn;
+
+  const dateOptions = {
+    month: 'long',
+    day: 'numeric',
+  };
+  const date = new Date(releaseDate).toLocaleDateString('ru-Ru', dateOptions);
+
+  filmReleaseDate.textContent = date;
+
+  filmCard.append(filmLink);
+  filmLink.append(filmImgWrap, filmTitle, filmReleaseDate);
+  filmImgWrap.append(filmPoster, filmDetails);
+  filmDetails.append(filmRating, filmGenres, filmDuration);
+
+  return filmCard;
+};
+
+async function createApp() {
+  // get query-params
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop)
+  });
+
+  let queryParam = params.page;
+
+  if (!queryParam) {
+    queryParam = 1;
+    history.pushState(null, null, '?page=1');
+  };
+
+  const monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const monthRu = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+
+  const container = document.querySelector('.app__container');
+  const appTitle = document.createElement('h1');
+  const month = document.createElement('span');
+  const year = document.createElement('span');
+  const list = document.createElement('ul');
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth();
+  const currentMonthText = monthArr[currentMonth].toUpperCase();
+  const currentMonthTextRu = monthRu[currentMonth];
+
+  appTitle.classList.add('app__title');
+  appTitle.textContent = 'Цифровые релизы фильмов ';
+  list.classList.add('list-reset', 'app__list');
+  month.classList.add('month');
+  month.textContent = `${currentMonthTextRu} `;
+  year.classList.add('year');
+  year.textContent = currentYear;
+
+  appTitle.append(month, year);
+
+  container.append(appTitle, list);
+
+  const data = await getFilmsData(currentMonthText, currentYear, queryParam);
+
+  list.before(createPagination(data.total, queryParam));
+  list.after(createPagination(data.total, queryParam));
+
+  data.releases.forEach(item => {
+    const filmCard = createFilmCard(item);
+    list.append(filmCard)
+  });
+};
+
+async function getFilmsData(month, year, page) {
+  const API_KEY = 'cda4fa97-c203-4818-b207-28de27eb0201';
+  const response = await fetch(`https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=${year}&month=${month}&page=${page}`, {
     headers: {
       "Content-Type": "application/json",
       "X-API-KEY": API_KEY,
     }
-  })
-  .then((response) => {
-    if (response.status === 404) {
-      throw Error('По данному запросу ничего не найдено :(');
-    } else if (response.status === 500) {
-      throw Error('Произошла ошибка, пожалуйста, попробуйте обновить страницу позже');
-    };
-
-    return response.json();
-  })
-  .then((data) => {
-    errorEl.style.display = 'none';
-    loaderEl.style.display = 'none';
-
-    //get num of pages for pagination
-    const total = data.total;
-    const pages = Math.ceil(total / 10);
-
-    for (let i = pages; i >= 1; i--) {
-      paginations.forEach(el => {
-        el.insertAdjacentHTML('afterbegin', `
-          <li class="app__pagination-item">
-            <a href="?page=${i}" class="app__pagination-link ${i == queryParam ? 'app__pagination-link--current' : ''}">${i}</a>
-          </li>
-        `);
-      });
-    };
-
-    return data;
-  })
-  .then((data) => {
-    for (release of data.releases) {
-      const rating = release.rating ? release.rating.toFixed(1) : 'Недостаточно голосов';
-      const genres = release.genres.map(genre => Object.values(genre)[0]).toString().replace(/,/g, ', ');
-      const duration = `${release.duration} ${seclOfNum(release.duration, ['минута', 'минуты', 'минут'])}`;
-      const dateOptions = {
-        month: 'long',
-        day: 'numeric',
-      };
-      const date = new Date(release.releaseDate).toLocaleDateString('ru-Ru', dateOptions);
-
-      appList.insertAdjacentHTML('beforeend', `
-        <li class="app__list-item">
-          <article class="app__card film-card">
-            <a href="https://kinopoisk.ru/film/${release.filmId}" target="_blank" class="film-card__link">
-              <div class="film-card__img-wrap">
-                <img src="${release.posterUrlPreview}" alt="${release.nameRu ? release.nameRu : release.nameEn}" loading="lazy" class="film-card__img">
-                <div class="film-card__details">
-                  <div class="film-card__rating ${release.rating == null ? 'film-card__rating--null' : ''}">${rating}</div>
-                  <div class="film-card__genres">${genres}</div>
-                  <div class="film-card__duration ${release.duration == 0 ? 'film-card__duration--hidden' : ''}">${duration}</div>
-                </div>
-              </div>
-              <h2 class="film-card__title">${release.nameRu ? release.nameRu : release.nameEn}</h2>
-              <div class="film-card__release-date">${date}</div>
-            </a>
-          </article>
-        </li>
-      `);
-    }
-  })
-  .catch((err) => {
-    console.log(err.name)
-    if (err.name === 'TypeError') {
-      errorEl.textContent = 'Введен неверный адрес';
-    } else {
-      errorEl.textContent = err.message;
-    }
-    errorEl.style.display = 'block';
-    loaderEl.style.display = 'none';
-  })
-
-
+  });
+  return await response.json();
 };
 
-initApp(queryParam);
+createApp();
